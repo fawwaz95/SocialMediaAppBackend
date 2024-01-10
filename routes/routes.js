426a8460fe90e728c989b77cad2e830a16553af5
fawwaz95 <fawwaz_95@hotmail.com>
2024-01-09 22:34:33 -0500

    working on login endpoint logic

--- routes/routes.js
+++ routes/routes.js
@@ -3,22 +3,26 @@ const getDBConnection = require("../db/conn.js");
 const data = require("../db/data.js");
 const router = express.Router();
 
+router.get("/", async (req, res) => {
+  const insertUsers = await data();
+  console.log("Inserted a user........" + insertUsers);
+  res.send("Created a user....").status(200);
+});
 
 router.get("/getUsers", async (req, res) => {
-  const insertUsers = await data();
-  console.log("The data............." + insertUsers);
-
   const db = await getDBConnection();
   const usersCollection = await db.collection("users");
   const results = await usersCollection.find({}).toArray();
   res.json(results).status(200);
 });
 
-router.post("/createUser", async (req, res) => {
-  /*console.log("The user req to create......." + req.userName);
+router.post("/login", async (req, res) => {
   const db = await getDBConnection();
-  const usersCollection = await db.collection("users");*/
-  res.send("createUser endpoint hit!").status(200);
+  const findUser = await db.collection("users").find({userName: req.params.userName, password: req.params.password}).toArray();
+
+  console.log("Does the user exist? " + findUser);
+
+  res.send(findUser).status(200);
 });
 
 module.exports = router;
