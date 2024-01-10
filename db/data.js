1bf97a4b14d4152d57ea031e079cdc3af4c4de3e
fawwaz95 <fawwaz_95@hotmail.com>
2024-01-09 22:10:23 -0500

    establish connection to users collection and inserting first user data

--- db/data.js
+++ db/data.js
@@ -1,23 +1,28 @@
-const db = require("./conn.js");
+const getDBConnection = require("./conn.js");
 
 const data = async () => {
-    const collection = db.collection("users");
-    const userRecords = [
-      {
-        _id: "USER0001",
-        firstName: "John",
-        lastName: "Doe",
-        userName: "JohnDoe",
-        email: "fawwaz.ahmad@zcomsystems.com",
-        password:"password"
-      },
-    ];
+    try {
+      const db = await getDBConnection();
+      const usersCollection = await db.collection("users");
+      const userRecords = [
+        {
+          _id: "USER0001",
+          firstName: "John",
+          lastName: "Doe",
+          userName: "JohnDoe",
+          email: "fawwaz.ahmad@zcomsystems.com",
+          password: "password"
+        },
+      ];
 
-    const delRecord = {};
-    const del = await collection.deleteMany(delRecord);
-    console.log(`${del.deletedCount} documents deleted`);
-    const result = await collection.insertMany(userRecords);
-    console.log(`${result.insertedCount} documents inserted`);
-}
+      const delRecord = {};
+      const del = await usersCollection.deleteMany(delRecord);
+      console.log(`${del.deletedCount} documents deleted`);
+      const result = await usersCollection.insertMany(userRecords);
+      console.log(`${result.insertedCount} documents inserted`);
+    } catch (error) {
+      console.error("Error seeding data:", error);
+    }
+  }
 
-module.exports = data;
+  module.exports = data;
