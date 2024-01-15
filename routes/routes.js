d047ccc76bfb56fe3dc07f70c34932aee267f4c2
fawwaz95 <fawwaz_95@hotmail.com>
2024-01-14 21:12:42 -0500

    aded /register endpoint added encryption changed /login endpoint, able to check if user exist able to register user...still need to make more changes here

--- routes/routes.js
+++ routes/routes.js
@@ -3,11 +3,39 @@ const getDBConnection = require("../db/conn.js");
 const data = require("../db/data.js");
 const router = express.Router();
 
-router.get("/", async (req, res) => {
+const bcrypt = require("bcrypt");
+
+const checkIfUserExists = async (username, password) => {
+  const db = await getDBConnection();
+  const user = await db.collection('users').findOne({ userName: username });
+  return user;
+}
+
+const registerUser = async (firstName, lastName, userName, email, password) => {
+
+  if (!firstName || !lastName || !userName || !email || !password) {
+    return { success: false, message: "All fields are required." };
+  }
+
+  const hashPassword = await bcrypt.hash(password, 12);
+
+  const db = await getDBConnection();
+  const insertUser = await db.collection("users").insertOne({firstName: firstName, lastName: lastName, userName: userName, email: email, password: hashPassword});
+
+  if(insertUser && insertUser.insertedId){
+    console.log("User was created! " + insertUser[0].userName);
+    return insertUser;
+  }else{
+    console.error("Error registering user......" + insertUser);
+    console.error(insertUser);
+  }
+}
+
+/*router.get("/", async (req, res) => {
   const insertUsers = await data();
   console.log("Inserted a user......." + insertUsers);
   res.send("Created a user....").status(200);
-});
+});*/
 
 router.get("/getUsers", async (req, res) => {
   const db = await getDBConnection();
@@ -17,12 +45,25 @@ router.get("/getUsers", async (req, res) => {
 });
 
 router.post("/login", async (req, res) => {
-  const db = await getDBConnection();
-  const findUser = await db.collection("users").find({userName: req.params.userName, password: req.params.password}).toArray();
+  const {userName, password} = req.query;
+  const doesUserExist = await checkIfUserExists(userName, password);
+  doesUserExist ?  res.send(doesUserExist).status(200) :  res.sendStatus(401);
+});
 
-  console.log("Does the user exist? " + findUser);
 
-  res.send(findUser).status(200);
+router.post("/register", async (req, res) => {
+  console.log("HIT?");
+  const {firstName, lastName, userName, email, password} = req.query;
+  const doesUserExist = await checkIfUserExists(userName, password);
+
+  if(doesUserExist){
+    console.log("Users already exist, logging in........");
+    return res.send("User exists.........").status(401);
+  }else{
+    console.log("Registering user........");
+    const userRegistered = await registerUser(firstName, lastName, userName, email, password);
+    return res.send(userRegistered).status(200);
+  }
 });
 
 module.exports = router;
